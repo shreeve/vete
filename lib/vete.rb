f0e9af1ac0a78fd3f32171bc88815c21aa429dbd
Steve Shreeve <steve.shreeve@gmail.com>
2023-03-21 22:12:38 -0600

    Bump to v0.3.0: Add support for delaying/staggering job launches

--- lib/vete.rb
+++ lib/vete.rb
@@ -21,7 +21,7 @@ trap("INT"  ) { print clear + go; abort "\n" }
 trap("WINCH") { print clear or draw if @pid == Process.pid }
 
 OptionParser.new.instance_eval do
-  @version = "0.2.0"
+  @version = "0.3.0"
   @banner  = "usage: #{program_name} [options]"
 
   on "-b", "--bar <width>"            , "Progress bar width, in characters", Integer
