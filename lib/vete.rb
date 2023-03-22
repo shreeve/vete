13df14a8b62c4b2a21041cc8e91df2ec973a9490
Steve Shreeve <steve.shreeve@gmail.com>
2023-03-22 04:59:44 -0600

    Bump to v.0.6.5: Improve the move() method

--- lib/vete.rb
+++ lib/vete.rb
@@ -19,7 +19,7 @@ trap("INT"  ) { print clear + go; abort "\n" }
 trap("WINCH") { print clear or draw if @pid == Process.pid }
 
 OptionParser.new.instance_eval do
-  @version = "0.6.4"
+  @version = "0.6.5"
   @banner  = "usage: #{program_name} [options]"
 
   on "-b", "--bar <width>"            , "Progress bar width, in characters", Integer
@@ -58,7 +58,7 @@ end
 @died = File.join(@vete, "died")
 
 def move(path, dest)
-  dest = File.join(dest, File.basename(path))
+  dest = File.join(dest, File.basename(path)) unless path.is_a?(Array)
   FileUtils.mv(path, dest, force: true, secure: true)
 end
 
