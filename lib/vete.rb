3a110510fa62e1e3834606b0c68948b02e6dedf6
Steve Shreeve <steve.shreeve@gmail.com>
2023-03-22 04:35:47 -0600

    Bump to v0.6.4: Allow workers to access original @opts

--- lib/vete.rb
+++ lib/vete.rb
@@ -19,7 +19,7 @@ trap("INT"  ) { print clear + go; abort "\n" }
 trap("WINCH") { print clear or draw if @pid == Process.pid }
 
 OptionParser.new.instance_eval do
-  @version = "0.6.3"
+  @version = "0.6.4"
   @banner  = "usage: #{program_name} [options]"
 
   on "-b", "--bar <width>"            , "Progress bar width, in characters", Integer
@@ -31,7 +31,7 @@ OptionParser.new.instance_eval do
   on "-w", "--workers <count>"        , "Set the number of workers (default is 1)", Integer
 
   self
-end.parse!(into: opts={}) rescue abort($!.message)
+end.parse!(into: @opts = opts = {}) rescue abort($!.message)
 
 # populate CLI options
 @char = opts[:char   ] || "•"; @char = @char[0]
