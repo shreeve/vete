96eefe7045bad49e5fd3d76aeb3decc46948c528
Steve Shreeve <steve.shreeve@gmail.com>
2023-07-01 03:22:17 -0600

    Bump to v0.6.8: Slightly improve the example

--- lib/vete.rb
+++ lib/vete.rb
@@ -19,7 +19,7 @@ trap("INT"  ) { print clear + go; abort "\n" }
 trap("WINCH") { print clear or draw if @pid == Process.pid }
 
 OptionParser.new.instance_eval do
-  @version = "0.6.7"
+  @version = "0.6.8"
   @banner  = "usage: #{program_name} [options]"
 
   on "-b", "--bar <width>"            , "Progress bar width, in characters", Integer
